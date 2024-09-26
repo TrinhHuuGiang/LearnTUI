a12ce6cfa3ddde8e414f862a304984783db9bdb2
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-09-26 09:55:13 +0700

    end_NC_BS

---
+++ _2_Ncurses_Basic/main.c
@@ -0,0 +1,49 @@
+// About: initscr, endwin, refresh, getch, printw
+/********************************************************************************
+* Definitions
+********************************************************************************/
+#include <ncurses.h>
+/********************************************************************************
+* Variables
+********************************************************************************/
+
+/********************************************************************************
+* Prototypes
+********************************************************************************/
+
+/********************************************************************************
+* Code
+********************************************************************************/
+int main(int argc, char** argv)
+{
+    char temp = 0;
+
+    //Open a new interface on the terminal
+    //Set up memory for this screen
+    initscr();
+
+    //update what changed
+    refresh();
+
+    //print "hello world"
+    printw("Hello World!!\nPress a key: ");
+
+    //get a value user type
+    temp = getch();
+    while (getch()!='\n')
+    {
+        /* No code */
+    }
+    
+    //print the word on to screen
+    printw("%c", temp);
+
+    //stop screen
+    printw("\nPress any key to end!!");
+    getch();
+
+    //deallocate screen before end program
+    //return shell terminal
+    endwin();
+    return 0;
+}
