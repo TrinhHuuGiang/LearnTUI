b78fa7975fc793d1004e3767fd2a17e0818d17ba
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-10-17 16:40:37 +0700

    u-l

--- _1_HelloWorld/main.c
+++ _1_HelloWorld/main.c
@@ -1,3 +1,4 @@
+// This file is licensed under the GNU General Public License v3. See the end of this file for details.
 #include <stdio.h>
 
 int main(int argc,const char** argv)
@@ -16,3 +17,23 @@ int main(int argc,const char** argv)
 
     return 0;
 }
+
+/*
+    Copyright (C) <2024>  <TrinhHuuGiang>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact Information:
+    <giangtrinhvnbl2k2@gmail.com>
+*/
--- _7_Ncurses_Set_Color/main.c
+++ _7_Ncurses_Set_Color/main.c
@@ -1,3 +1,4 @@
+// This file is licensed under the GNU General Public License v3. See the end of this file for details.
 // About: Some color and style
 // style: attron, attroff
 // color: has_color, start_color, can_change_color, init_color, init_pair
@@ -83,3 +84,23 @@ int main()
     return 0;
 
 }
+
+/*
+    Copyright (C) <2024>  <TrinhHuuGiang>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact Information:
+    <giangtrinhvnbl2k2@gmail.com>
+*/
